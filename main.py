2e85caf054ca64e228eb98bccfab712a311d466a
Karan-424 <challengersince2004@gmail.com>
2025-07-02 07:15:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,3 +40,7 @@ def webhook():
     requests.post(telegram_url, json=payload)
 
     return "ok", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
